bd305198f1d1dde1a44c099449e005635ddf80de
dansilovsky <dan.silovsky@centrum.cz>
2014-06-12 19:17:10 +0100

    Added ArroundIterator

--- libs/Dan/Iterators/ArroundIterator.php
+++ libs/Dan/Iterators/ArroundIterator.php
@@ -3,19 +3,17 @@
 namespace Dan\Iterators;
 
 /**
- * ArroundArray iterates arround array. If starting position is set to non zero 
+ * AroundArray iterates around array. If starting position is set to non zero 
  * then it iterates from given position towards the top position 
  * then it returns to zero position and iterates towards starting position.
  * 
  * e.g array(a, b, c, d) with start position set to 2 would print out c, d, a, b 
  * 
- *   
- * 
  * @author Daniel Silovsky
  * @copyright (c) 2013, Daniel Silovsky
  * @license http://www.screwfix-calendar.co.uk/license
  */
-class ArroundIterator implements \Iterator {
+class AroundIterator implements \Iterator {
 	
 	protected $array;
 	
@@ -70,11 +68,12 @@ class ArroundIterator implements \Iterator {
 	{
 		return isset($this->array[$this->position]) && $this->top >= $this->movesCount;
 	}
+	
 	/**
 	 * Sets start position
 	 * 
 	 * @param int $position
-	 * @throws \Dan\ArroundIterator_OutOfScope_Exception
+	 * @throws \Dan\AroundIterator_OutOfScope_Exception
 	 */
 	function setStart($position)
 	{		
@@ -82,7 +81,7 @@ class ArroundIterator implements \Iterator {
 
 		if (!isset($this->array[$this->position]))
 		{
-			throw new \Dan\ArroundIterator_OutOfScope_Exception('Given start position is out of scope.');
+			throw new \Dan\AroundIterator_OutOfScope_Exception('Given start position is out of scope.');
 		}		
 	}
 }
