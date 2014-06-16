414d2cfb478752cf3508078a1ee0207ad81b32a3
dansilovsky <dan.silovsky@centrum.cz>
2014-06-16 12:59:10 +0100

    BaseaccountForm overrides getName() method so that it alway return the same name. It causes that all instances of Baseaccount form filled with controls create inputs with id's that contain this name.

--- app/components/BaseaccountForm.php
+++ app/components/BaseaccountForm.php
@@ -10,11 +10,18 @@ namespace Screfix;
  * @license http://www.screwfix-calendar.co.uk/license
  */
 class BaseaccountForm extends \Nette\Application\UI\Form {
+	
+	private $_name = 'baseaccountForm';
 
 	public function __construct(\Nette\ComponentModel\IContainer $parent = NULL, $name = NULL)
 	{
 		parent::__construct($parent, $name);
 		
 		$this->getElementPrototype()->id = 'frm-baseaccountForm';
+	}
+	
+	public function getName()
+	{
+		return $this->_name;
 	}
 }
