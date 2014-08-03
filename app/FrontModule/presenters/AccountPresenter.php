a5de8eee52b00867aa795c509ffd9b63db65315b
dansilovsky <dan.silovsky@centrum.cz>
2014-08-03 18:19:46 +0100

    AccountPresenter does not need inherit removed methods (createComponentCredentialsForm(), createComponentSetupForm(), setupFormSubmitted()) from BaseaccountPresenter. They are unique to AccountPresenter

--- app/FrontModule/presenters/AccountPresenter.php
+++ app/FrontModule/presenters/AccountPresenter.php
@@ -15,7 +15,32 @@ class AccountPresenter extends BaseaccountPresenter {
 	
 	protected function createComponentCredentialsForm() 
 	{
-		$form = parent::createComponentCredentialsForm();
+		$form = new BaseaccountForm($this, 'credentialsForm');
+		
+		$form->addSelect('select', 'Select', array('option1' => 'option1', 'option2' => 'option2',));
+		$form->addCheckbox('remember', 'Remember me');
+		
+		$form->addText('username', null, 30, 30)
+			->setAttribute('placeholder', 'Username')
+			->setAttribute('value', $this->identity->username)
+			->addRule(Form::MIN_LENGTH, 'Username must contain at least %d characters.', 3)
+			->addRule(Form::MAX_LENGTH, 'Username is too long. Use maximum of %d characters.', 60)
+			->addRule(Form::PATTERN, 'Username can contain only alphabetical characters or underscore.', '\w{3,60}');
+		$form->addText('email', 'Email', 30, 30)
+			->setAttribute('placeholder', 'Email')
+			->setRequired('Enter an email please.')
+			->addRule(Form::MAX_LENGTH, 'Email is too long. Use maximum of %d characters.', 255)
+			->addRule(Form::EMAIL, 'Invalid email address.');
+		$form->addPassword('password', null, 30)
+			->setAttribute('placeholder', 'Password')
+			->setRequired('Enter a password please.')
+			->addRule(Form::MIN_LENGTH, 'Password must contain at least %d characters.', 6);
+		$form->addPassword('verifyPassword', null, 30)
+			->setAttribute('placeholder', 'Retype password')
+			->setRequired('Reenter a password please.')
+			->addRule(Form::EQUAL, 'Passwords do not match.', $form['password']);
+		$form->addProtection('Time limit has expired. Please send the form again.', 1800);
+		$form->onSuccess[] = $this->credentialsFormSubmitted;
 		
 		$form->addSubmit('edit', 'Edit')
 			->setAttribute('class', 'button');
--- app/FrontModule/presenters/BaseaccountPresenter.php
+++ app/FrontModule/presenters/BaseaccountPresenter.php
@@ -25,68 +25,8 @@ abstract class BaseaccountPresenter extends BasePresenter {
 	/** @var \Screwfix\ShiftPatternFilterFactory @inject */
 	public $shiftPatternFilterFactory;
 	
-	protected function createComponentCredentialsForm() 
-	{
-		$form = new BaseaccountForm($this, 'credentialsForm');
-		
-		$form->addSelect('select', 'Select', array('option1' => 'option1', 'option2' => 'option2',));
-		$form->addCheckbox('remember', 'Remember me');
-		
-		$form->addText('username', null, 30, 30)
-			->setAttribute('placeholder', 'Username')
-			->setAttribute('value', $this->identity->username)
-			->addRule(Form::MIN_LENGTH, 'Username must contain at least %d characters.', 3)
-			->addRule(Form::MAX_LENGTH, 'Username is too long. Use maximum of %d characters.', 60)
-			->addRule(Form::PATTERN, 'Username can contain only alphabetical characters or underscore.', '\w{3,60}');
-		$form->addText('email', 'Email', 30, 30)
-			->setAttribute('placeholder', 'Email')
-			->setRequired('Enter an email please.')
-			->addRule(Form::MAX_LENGTH, 'Email is too long. Use maximum of %d characters.', 255)
-			->addRule(Form::EMAIL, 'Invalid email address.');
-		$form->addPassword('password', null, 30)
-			->setAttribute('placeholder', 'Password')
-			->setRequired('Enter a password please.')
-			->addRule(Form::MIN_LENGTH, 'Password must contain at least %d characters.', 6);
-		$form->addPassword('verifyPassword', null, 30)
-			->setAttribute('placeholder', 'Retype password')
-			->setRequired('Reenter a password please.')
-			->addRule(Form::EQUAL, 'Passwords do not match.', $form['password']);
-		$form->addProtection('Time limit has expired. Please send the form again.', 1800);
-		$form->onSuccess[] = $this->credentialsFormSubmitted;
-		
-		return $form;
-	}
-	
 	public function credentialsFormSubmitted(Form $form) {
 		
-	}
-	
-	protected function createComponentSetupForm() {
-		$sysPatternSelection = $this->sysPatternFacade->getFormSelection();	
-		
-		$form = new BaseaccounForm($this, 'setupForm');
-		
-		$form->getElementPrototype()->id = $this->commonFormId;
-		
-		$form->addSelect('sysPatternSelect', 'Select pattern', $sysPatternSelection);
-		
-		reset($sysPatternSelection);
-		$defaultPattern = $this->buildDefaultInputPattern(\Nette\Utils\Json::decode(key($sysPatternSelection)));
-		
-		$form['patternInput'] = $this->patternInputFactory->create();
-		$form['patternInput']->setDefaultValue($defaultPattern);
-		
-		$form->addSubmit('send', 'Send')
-			->setAttribute('class', 'button');
-		
-		$form->onSuccess[] = $this->setupFormSubmitted;
-		
-		return $form;
-	}
-	
-	public function setupFormSubmitted() 
-	{
-
 	}
 	
 	/**
